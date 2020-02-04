1e480304979abdbef335f9dbde7f397189576657
FelipeR <felipedaniel96@hotmail.com>
2020-02-04 06:47:10 -0300

    Projeto enviado FDR

---
+++ ControlePensao/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ControlePensao {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite a quantidade de quartos:");
+            int n = int.Parse(Console.ReadLine());
+
+            Quartos[] Q = new Quartos[10];
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("Digite o nome:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Digite o email:");
+                string email = Console.ReadLine();
+                Console.WriteLine("Digite o numero do quarto:");
+                int numQuarto = int.Parse(Console.ReadLine());
+
+                Q[numQuarto] = new Quartos(name,email, numQuarto);
+            }
+            Console.WriteLine("Quartos Ocupados:");
+            for (int i = 0; i < 10; i++) {
+
+                if (Q[i]!= null) {
+                    Console.Write(Q[i]);
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ ControlePensao/Quartos.cs
@@ -0,0 +1,19 @@
+﻿namespace ControlePensao {
+    class Quartos {
+
+        public string nome { get; set; }
+        public string email{ get; set; }
+        public int numQuartos { get; set; }
+
+        public Quartos(string _nome,string _email, int _numquarto) {
+            nome = _nome;
+            email = _email;
+            numQuartos = _numquarto;
+        }
+
+        public override string ToString() {
+            return numQuartos+","+nome+","+email;
+        }
+
+    }
+}
